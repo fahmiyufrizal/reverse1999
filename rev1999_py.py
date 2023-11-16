9f74a073eff4ed70fcd83d082b891f7f70f9fb9c
Fahmi Yufrizal Yusuf <60032856+fahmiyufrizal@users.noreply.github.com>
2023-11-17 05:20:14 +0700

    Add files via upload

---
+++ rev1999_py.py
@@ -0,0 +1,19 @@
+# Reverse1999 Global NetCafe Launcher
+# by fahmiyufrizal@2023
+
+import os
+import subprocess
+from os import path
+from sys import exit
+import asyncio
+import pathlib
+import configparser
+
+#censored
+#launch
+print("[+] Starting Reverse1999....")
+os.system('start "" reverse1999-launcher.exe')
+async def display():
+	await asyncio.sleep(5)
+asyncio.run(display())
+exit()
